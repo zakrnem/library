d340e153e90ebeb79d6807ba2c111452db0a1caa
zakrasty <zakrnem@zohomail.com>
2022-10-17 19:01:32 -0600

    Initial "add book" functionality

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function Book(title, author, pages, read) {
   this.pages = pages
   this.read = read
   this.sayInfo = function() {
-    console.log(`${title}, ${author}, ${pages}, ${read}.`)
+    return `${title}, ${author}, ${pages}, ${read}.`;
    }
 }
 
@@ -15,28 +15,15 @@ const book1 = new Book("48 Laws of Power", "Robert Greene", 452, "Not read");
 const book2 = new Book("Man's Search for Meaning", "Viktor Frankl", 188, "Read");
 
 
-// Object.create Syntax
-/* const Book = {
-  title: this.title,
-  author: this.author,
-  pages: this.pages,
-  read: this.read,
-  sayInfo: function() {
-    console.log(`${this.title}, ${this.author}, ${this.pages}, ${this.read}.`);
-  }
-};
 
-const book1 = Object.create(Book);
-book1.title = '48 Laws of Power';
-book1.author = 'Robert Greene';
-book1.pages = 485;
-book1.read = 'Not read';
-
-const book2 = Object.create(Book);
-book2.title = `Man's Search for Meaning`;
-book2.author = 'Viktor Frankl';
-book2.pages = 188;
-book2.read = 'Read'; */
+addBookButton = document.querySelector('.add-book');
+addBookButton.addEventListener('click', () => {
+  container = document.querySelector('container');
+  book3 = document.createElement('div');
+  book3.className = 'book';
+  book3.textContent = book1.sayInfo();
+  container.appendChild(book3);
+})
 
 function addBookToLibrary() {
   // do stuff here
