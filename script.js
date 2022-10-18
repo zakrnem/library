59428de1ae0d5a1c9dbcfed219a3e74d238cf114
zakrasty <zakrnem@zohomail.com>
2022-10-17 19:50:01 -0600

    Add book from defined variables

--- script.js
+++ script.js
@@ -14,17 +14,35 @@ function Book(title, author, pages, read) {
 const book1 = new Book("48 Laws of Power", "Robert Greene", 452, "Not read");
 const book2 = new Book("Man's Search for Meaning", "Viktor Frankl", 188, "Read");
 
-
+container = document.querySelector('container');
 
 addBookButton = document.querySelector('.add-book');
+
 addBookButton.addEventListener('click', () => {
-  container = document.querySelector('container');
   book3 = document.createElement('div');
   book3.className = 'book';
-  book3.textContent = book1.sayInfo();
+  book3.innerHTML = 
+    `<div class="book-buttons">
+    <button class="edit-button"><img src="edit-svgrepo-com.svg" alt="Edit" class="edit"></button>
+    <button class="remove-button"><img src="remove-svgrepo-com.svg" alt="Remove" class="remove"></button>
+    </div>
+    <div class="title">${book1.title}</div>
+    <div class="author">${book1.author}</div>
+    <div class="pages">${book1.pages}</div>
+    <div class="read">${book1.read}</div>`;
   container.appendChild(book3);
+});
+
+removeBookButton = document.getElementsByClassName('remove-button');
+
+removeBookButton.addEventListener('click', () => {
+  console.log('Remove')
 })
 
 function addBookToLibrary() {
   // do stuff here
+  book3 = document.createElement('div');
+  book3.className = 'book';
+  book3.textContent = book1.sayInfo();
+  container.appendChild(book3);
 }
